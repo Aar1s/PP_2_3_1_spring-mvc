d3d78992f6d02e81c4ab56d0ccfad3ba118e5268
Aar1s <65885326+Aar1s@users.noreply.github.com>
2022-05-26 18:40:32 +0500

    Second Try

--- src/main/java/web/DAO/UserDAOImpl.java
+++ src/main/java/web/DAO/UserDAOImpl.java
@@ -8,7 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
-
+//Hasitworked?
 @Repository
 public class UserDAOImpl implements UserDAO {
 
